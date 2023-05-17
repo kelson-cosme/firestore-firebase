3a7756bdfc6b706c9d0c44284bb087218bed62c7
kelson-cosme <kelson.almeida123@gmail.com>
2023-05-16 22:49:00 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { } from 'https://www.gstatic.com/firebasejs/9.21.0/firebase-firestore.js'
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDu9roM1q92Rfvu87eoa-Qnes_6EuajPVc",
+  authDomain: "colegio-e6fc3.firebaseapp.com",
+  projectId: "colegio-e6fc3",
+  storageBucket: "colegio-e6fc3.appspot.com",
+};
 
 function App() {
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+async function getAlunos(db) {
+  const alunos = collection(db, 'turmaA');
+  const alunosSnapshot = await getDocs(alunos);
+  const alunoLista = alunosSnapshot.docs.map(doc => doc.data());
+    alunoLista.forEach((doc) => { //ler os dados de todas coleção
+      console.log(doc.nome)
+    })
+}
+getAlunos(db)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      teste
     </div>
   );
 }
